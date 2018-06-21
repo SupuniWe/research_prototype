9591b5fd2e84ee48d51b673ec30cd0f609e5c7b4
Supuni Weerasingha <supuni.weerasingha@ifsworld.com>
2018-06-21 19:07:41 +0530

    initial commit

---
+++ app/src/main/java/com/example/asus/suppa/Camara.java
@@ -0,0 +1,55 @@
+package com.example.asus.suppa;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+
+public class Camara extends AppCompatActivity {
+    ImageView imgView;
+    byte[] image;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camara);
+
+        Button btnCamara = (Button) findViewById(R.id.btnCamera);
+        imgView = (ImageView) findViewById(R.id.img2);
+
+
+        btnCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+//        super.onActivityResult(requestCode, resultCode, data);
+//        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+//        imageView.setImageBitmap(bitmap);
+
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        image = stream.toByteArray();
+        imgView.setImageBitmap(bitmap);
+
+    }
+}
